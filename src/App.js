11471cbb8588efc95ec71768d96cf5f499dc6a1a
BuildTools <jsimonroyer@gmail.com>
2021-07-05 20:17:47 -0400

    bg change fix

--- src/App.js
+++ src/App.js
@@ -5,85 +5,96 @@ import cv from "./cv.PNG";
 import {
   Loader,
   OrbitControls,
-  useGLTF,
   PerspectiveCamera,
   Stars,
 } from "@react-three/drei";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-function Model(props) {
-  const group = useRef();
-  const { nodes, materials } = useGLTF("/mac-draco.glb");
-  return (
-    <group ref={group} {...props} dispose={null}>
-      <mesh castShadow receiveShadow />
-      <mesh castShadow receiveShadow />
-    </group>
-  );
-}
-
-useGLTF.preload("/mac-draco.glb");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Canvas dpr={[1.5, 2]} linear shadows>
+          <fog attach="fog" args={["#272730", 16, 30]} />
+          <ambientLight intensity={0.75} />
+          <PerspectiveCamera makeDefault position={[0, 0, 16]} fov={75}>
+            <pointLight intensity={1} position={[-10, -25, -10]} />
+            <spotLight
+              castShadow
+              intensity={2.25}
+              angle={0.2}
+              penumbra={1}
+              position={[-25, 20, -15]}
+              shadow-mapSize={[1024, 1024]}
+              shadow-bias={-0.0001}
+            />
+          </PerspectiveCamera>
+          <OrbitControls
+            autoRotate
+            enablePan={false}
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Stars radius={500} depth={50} count={1500} factor={10} />
+        </Canvas>
         <h1>Jean-Simon Royer</h1>
-        <a
-          href="https://www.github.com/jsip"
-          target="_blank"
-          rel="noreferrer"
-          style={{ "padding-top": "2rem", zIndex: "99" }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="32"
-            height="32"
+        <div id="canvas">
+          <a
+            href="https://www.github.com/jsip"
+            target="_blank"
+            rel="noreferrer"
+            style={{ "padding-top": "2rem", zIndex: "99" }}
           >
-            <path
-              fill-rule="evenodd"
-              fill="#fff"
-              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-            ></path>
-          </svg>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jean-simon-royer-453a38196/"
-          target="_blank"
-          rel="noreferrer"
-          style={{ paddingTop: "2rem", zIndex: "99" }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            width="32"
-            height="32"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              width="32"
+              height="32"
+            >
+              <path
+                fill-rule="evenodd"
+                fill="#fff"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+              ></path>
+            </svg>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jean-simon-royer-453a38196/"
+            target="_blank"
+            rel="noreferrer"
+            style={{ paddingTop: "2rem", zIndex: "99" }}
           >
-            <path
-              fill="#0288D1"
-              d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
-            />
-            <path
-              fill="#FFF"
-              d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
-            />
-          </svg>
-        </a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 48 48"
+              width="32"
+              height="32"
+            >
+              <path
+                fill="#0288D1"
+                d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
+              />
+              <path
+                fill="#FFF"
+                d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
+              />
+            </svg>
+          </a>
+        </div>
       </header>
+      <img src={cv} alt="cv" class="cv-img"></img>
       <div>
-        <img src={cv} alt="cv" style={{}}></img>
         {/* 
         <div class="cv-cont">
-          <div class="grid-left">
+        <div class="grid-left">
             <div class="rows-cont">
-              <div class="row1">
+            <div class="row1">
                 <h4>Jean-Simon Royer</h4>
-              </div>
-              <div class="row2">
+                </div>
+                <div class="row2">
                 <h4>Expérience</h4>
-              </div>
-              <div class="row3">
+                </div>
+                <div class="row3">
                 <h4>Compétences</h4>
                 <ul>
                   <li>Bilingue écrit/parlé</li>
@@ -295,33 +306,6 @@ function App() {
       */}
       </div>
       <section id="canvas">
-        <Canvas dpr={[1.5, 2]} linear shadows>
-          <fog attach="fog" args={["#272730", 16, 30]} />
-          <ambientLight intensity={0.75} />
-          <PerspectiveCamera makeDefault position={[0, 0, 16]} fov={75}>
-            <pointLight intensity={1} position={[-10, -25, -10]} />
-            <spotLight
-              castShadow
-              intensity={2.25}
-              angle={0.2}
-              penumbra={1}
-              position={[-25, 20, -15]}
-              shadow-mapSize={[1024, 1024]}
-              shadow-bias={-0.0001}
-            />
-          </PerspectiveCamera>
-          <Suspense fallback={null}>
-            <Model />
-          </Suspense>
-          <OrbitControls
-            autoRotate
-            enablePan={false}
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Stars radius={500} depth={50} count={1500} factor={10} />
-        </Canvas>
         <div className="layer" />
         <Loader />
       </section>
